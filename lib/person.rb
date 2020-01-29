3cf9c23b6a499a3e7e61297cf684c1b0fe2ca97b
JasonP77 <llvslr77@gmail.com>
2020-01-29 10:15:18 -0500

    Finished #1 on Non-Attr

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,49 @@
 # your code goes here
+class Person
+attr_reader :name, :happiness, :hygiene
+attr_accessor :bank_account
+
+def initialize(name)
+	@name = name
+	@bank_account = 25
+	@happiness = 8
+	@hygiene = 8
+end
+
+
+def clean?
+	if @hygiene > 7
+		return true
+	else false
+	end
+end
+
+def happy?
+	if @happiness > 7
+		return true
+	else false
+	end
+end
+
+def happiness=(happiness)
+	if happiness > 10
+		@happiness = 10
+	elsif happiness < 0
+		@happiness = 0
+	else 
+		@happiness = happiness
+	end
+end
+
+def hygiene=(hygiene)
+	if hygiene > 10
+		@hygiene = 10
+	elsif hygiene < 0
+		@hygiene = 0
+	else 
+		@hygiene = hygiene
+	end
+end
+
+
+end
